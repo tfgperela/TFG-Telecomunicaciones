3ef397a27eae39566ebf2727b9931d3994a9489f
tfgteleco <tfgteleco@gmail.com>
2021-03-03 10:07:38 +0100

    Añadido nuevo modelo en R para nuevo modelo PLS-PM

--- modelo R/tfg_test_new.R
+++ modelo R/tfg_test_new.R
@@ -1,25 +1,26 @@
 library(plspm)
 #library(ggplot2)
-mydata = read.csv("/home/rodrigo/Documentos/tfg-teleco/TFG-Telecomunicaciones/indices/todo/ad_0.csv", header = TRUE)
+mydata = read.csv("/home/rodrigo/Documentos/tfg-teleco/TFG-Telecomunicaciones/indices/todo/prueba/ad_0_mujeres.csv", header = TRUE)
 
 head(mydata, n = 5)
 
-Parasimpatico = c(0, 0, 0, 0)
+Vagal = c(0, 0, 0, 0)
 Simpatico = c(0, 0, 0, 0)
 ANS = c(1, 1, 0, 0)
 Recall = c(0, 0, 1, 0)
-ad_path = rbind(Parasimpatico, Simpatico, ANS, Recall)
+ad_path = rbind(Vagal, Simpatico, ANS, Recall)
 colnames(ad_path) = rownames(ad_path)
 
 innerplot(ad_path)
 
 #Higher-order constructs in PLS-PM: 1) Repeated Indicators Approach
 #Posible interesante usar 3) Hybrid Approach
-ad_blocks = list(c(5, 6, 9), c(2, 7, 8), c(3,10), 11)
+ad_blocks = list(c(5, 6, 9), c(2, 7, 8, 12), c(3,10), 11)
 #Los indicadores de "EDA" y "Score" son formative ("B")
 ad_modes = c("A", "A", "A", "A")
 
-ad_pls = plspm(mydata, ad_path, ad_blocks, modes = ad_modes)
+#ad_pls = plspm(mydata, ad_path, ad_blocks, modes = ad_modes)
+ad_pls = plspm(mydata, ad_path, ad_blocks, modes = ad_modes, maxiter = 500)
 plot(ad_pls)
 #plot(ad_pls, what = "loadings", arr.width = 0.1)
 outerplot(ad_pls)
